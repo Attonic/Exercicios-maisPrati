0a0e8429b2a4b5ed55fe6a98b6a28897f942b532
Antonio Cleison <a.cleisonn@gmial.com>
2025-05-03 15:53:41 -0300

    Adiciona restante dos exercicios

--- exercicios01/12-tabuada.js
+++ exercicios01/12-tabuada.js
@@ -1,1 +1,25 @@
 // 12. Crie um programa que exibe a tabuada de um número fornecido pelo usuário (de 1 a 10) utilizando um loop for.
+
+const prompt = require("prompt-sync")();
+
+let num = Number(prompt("Digite um numero de 1 a 10: "));
+
+console.log(`Tabuada de Soma ${num}`);
+for(let i = 1; i <= 10; i++){
+    console.log(`${num} + ${i} = ${i + num}`);
+}
+
+console.log(`Tabuada de Subtração ${num}`);
+for(let i = 1; i <= 10; i++){
+    console.log(`${num} + ${i} = ${i - num}`);
+}
+
+console.log(`Tabuada de Divisão ${num}`);
+for(let i = 1; i <= 10; i++){
+    console.log(`${num} / ${i} = ${i / num}`);
+}
+
+console.log(`Tabuada de Multiplicação ${num}`);
+for(let i = 1; i <= 10; i++){
+    console.log(`${num} * ${i} = ${i * num}`);
+}
--- exercicios01/14-fatorial.js
+++ exercicios01/14-fatorial.js
@@ -1,1 +1,15 @@
 // 14. Crie um programa que calcula o fatorial de um número fornecido pelo usuário utilizando um loop for ou while.
+
+const prompt = require("prompt-sync")();
+
+let num = Number(prompt("Digite um numero: "))
+
+function fatorialNumro(num){
+    let total = 1;
+    for(let i = 1; i <= num; i++){
+        total *= i;    
+    }
+    return total;
+}
+
+console.log(`O valor fatorial de ${num} é: ${fatorialNumro(num)}`);
